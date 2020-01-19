a722519ea932de63eec4d3817c4d3043084d032f
Kukuh Setya <chibi.m4ru@gmail.com>
2020-01-19 13:51:41 +0700

    menambahkan index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GPU TES REMOTE 2</title>
+</head>
+<body>
+    <H2>Hai Dunia</H2>
+</body>
+</html>
