aa1a9d8b6f505a24b63095e07a37d2b0d9946048
Kukuh Setya <chibi.m4ru@gmail.com>
2020-01-19 13:52:36 +0700

    menambahkan paragrap

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <H2>Hai Dunia</H2>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur distinctio commodi dignissimos iusto delectus saepe, officia quidem qui, iste temporibus totam repudiandae vitae blanditiis cum libero quis reiciendis nulla tenetur.</p>
 </body>
 </html>
